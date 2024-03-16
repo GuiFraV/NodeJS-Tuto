82a3cd14d9c874ff4d67d1fb4a02f919c1f90753
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-03-16 09:35:54 +0100

    Refactor code and add comments

--- algoday.js
+++ algoday.js
@@ -19,12 +19,12 @@ const titleBooks = books.map(book => book.title)
 const sortedBooksByYears = [...books].sort((a,b) => {
     return a.year - b.year;
 })
-console.log(sortedBooksByYears);
+// console.log(sortedBooksByYears);
 
 // Exercice 3 : Le livre le plus emprunté
 // Trouve le livre qui a été emprunté le plus de fois.
 let maxBorrows = 0;
-let mostBorrowedBook = null;
+let mostBorrowedBook = null;    
 
 books.forEach(book => {
     const borrowCount = book.borrowedBy.length;
@@ -34,10 +34,28 @@ books.forEach(book => {
     }
 });
 
-console.log(`Le livre le plus emprunté est "${mostBorrowedBook.title}" avec ${maxBorrows} emprunts.`);
+// console.log(`Le livre le plus emprunté est "${mostBorrowedBook.title}" avec ${maxBorrows} emprunts.`);
 
 // Exercice 4 : Nombre d'emprunts par auteur
 // Calcule le nombre total d'emprunts pour chaque auteur et affiche le résultat.
+
+let totalBorrow = {};
+
+books.forEach(book => {
+    console.log(book)
+    book.borrowedBy.forEach(borrow => {
+
+        // console.log(borrow)
+        if(totalBorrow[book.author]){
+            totalBorrow[book.author]++
+            console.log(totalBorrow[book.author])
+        }else{
+            totalBorrow[book.author] = 1;
+        }
+    })
+})
+
+console.log(totalBorrow);
 
 // Exercice 5 : Une fonction qui prend en paramètre un id de livre et renvoie une chaîne de caractères
 // La fonction doit renvoyer une chaîne de caractères qui décrit le livre et les personnes qui l'ont emprunté, par exemple : "Les Misérables, écrit par Victor Hugo en 1862, a été emprunté par les personnes avec les id."
